ca34a9d398a719b0828f0f79d3a011de4e1056f4
fumiyaaan <indestructible.peace@gmail.com>
2020-10-09 09:47:20 +0900

    画像のみ空の投稿をした場合のエラーを修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,7 @@ class Prototype < ApplicationRecord
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
+  # 上記imageカラムのバリデーション設定漏れにつき、追加した。
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_one_attached :image 
